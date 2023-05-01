dbcd0fd10089d4e04af4074ef05272d83552a871
SongSiHo <wt6248@gmail.com>
2023-05-01 21:11:56 +0900

    first version of game

---
+++ Assets/generateBullet.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class generateBullet : MonoBehaviour
+{
+    public int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void getScore()
+    {
+        score++;
+    }
+    
+}
